057c7d8e969ef1590444c2a9e950539bdeeb88b1
LimeSugarGlider <80134580+LimeSugarGlider@users.noreply.github.com>
2021-03-05 20:09:39 -0500

    Uploaded first version of MW6812 code
    Added MW6812 emulator code for later use

---
+++ fantasy16/data/mw6812.lua
@@ -0,0 +1,20 @@
+--Crude MW6812 emulator, requires input of clock (intended to increase by 1/44100 for every sample) and a large memory table containing five channel tables, each with a wavetable table and six registers
+--by lsg 3/5/21
+function mw6812( mw6812Clock , mw6812Reg )
+	mw6812OutL = 0
+	mw6812OutR = 0
+	--1 Carrier wavetable (16 entry table)
+	--2 Carrier frequency
+	--3 Carrier amplitude
+	--4 Sine modulator frequency
+	--5 Sine modulator amplitude
+	--6 Left ear enabled?
+	--7 Right ear enabled?
+	for mw6812Channel=1,5 do
+		mw6812ChannelOut = mw6812Reg[mw6812Channel][3] * mw6812Reg[mw6812Channel][1][math.floor( (mw6812Clock + ( mw6812Reg[mw6812Channel][5] * math.sin(2*math.pi*mw6812Reg[mw6812Channel][4]*mw6812Clock) )) / 0.0625 * mw6812Reg[mw6812Channel][2] ) % 16 + 1 ]
+		if (mw6812Reg[mw6812Channel][6] or mw6812Reg[mw6812Channel][6] == 1) then mw6812OutL = mw6812OutL + mw6812ChannelOut / 5 end
+		if (mw6812Reg[mw6812Channel][7] or mw6812Reg[mw6812Channel][7] == 1) then mw6812OutR = mw6812OutR + mw6812ChannelOut / 5 end
+	end
+	
+	return { mw6812OutL , mw6812OutR }
+end
